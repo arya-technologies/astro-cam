ee8f18e56476d5cb8c95f820fadc7d790befa83c
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-06 03:04:41 +0530

    added resolution selection

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,23 +4,54 @@ import {
   CameraType,
   useCameraPermissions,
   CameraMode,
+  CameraViewRef,
+  VideoQuality,
+  ImageType,
 } from "expo-camera";
 import { router } from "expo-router";
-import { useState } from "react";
-import { Dimensions, Pressable, TouchableOpacity, View } from "react-native";
-import { Button, IconButton, Surface, Text } from "react-native-paper";
+import { useEffect, useState } from "react";
+import {
+  Dimensions,
+  Pressable,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  Button,
+  IconButton,
+  Icon,
+  Surface,
+  Text,
+  Portal,
+  Dialog,
+  List,
+  RadioButton,
+} from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function index() {
   const { colors } = useAppTheme();
   const { top, bottom } = useSafeAreaInsets();
   const { width, height } = Dimensions.get("window");
+  const [permission, requestPermission] = useCameraPermissions();
 
-  const pictureSizes = CameraView;
+  const [isResDialogVisible, setisResDialogVisible] = useState<boolean>(false);
+  const showResDialog = () => setisResDialogVisible(true);
+  const hideResDialog = () => setisResDialogVisible(false);
 
   const [facing, setfacing] = useState<CameraType>("back");
   const [mode, setmode] = useState<CameraMode>("picture");
-  const [permission, requestPermission] = useCameraPermissions();
+  const [videoQuality, setvideoQuality] = useState<VideoQuality>("4:3");
+  const [camera, setcamera] = useState<CameraView | null>();
+  const [pictureSize, setpictureSize] = useState<string>("3000x3000");
+  const [pictureSizes, setpictureSizes] = useState<string[]>([]);
+  const [maxDuration, setmaxDuration] = useState<number>(30);
+  const [imageType, setimageType] = useState<ImageType>("png");
+
+  useEffect(() => {
+    camera?.getAvailablePictureSizesAsync().then((res) => setpictureSizes(res));
+  }, [camera]);
 
   if (!permission) {
     return <View />;
@@ -44,51 +75,95 @@ export default function index() {
     setfacing((current) => (current === "back" ? "front" : "back"));
   }
   function toggleCameraMode() {
-    setmode((current) => (current === "video" ? "video" : "picture"));
+    setmode((current) => (current === "picture" ? "video" : "picture"));
   }
-  function handleCapture() {
+  async function handleCapture() {
     if (mode === "picture") {
+      await camera?.takePictureAsync({
+        imageType,
+        quality: 1,
+        skipProcessing: true,
+      });
+    } else if (mode === "video") {
+      await camera?.recordAsync({ maxDuration });
     }
   }
 
   return (
-    <View
-      className="h-full flex-1 px-4"
-      style={{
-        backgroundColor: colors.surface,
-        paddingTop: top,
-        paddingBottom: bottom,
-      }}
-    >
-      <View style={{ height: height / 1.25 }}>
-        <Surface elevation={4} className="rounded-lg overflow-hidden h-full">
-          <CameraView
-            facing={facing}
-            mode={mode}
-            mute
-            className="w-full h-full"
-          />
-        </Surface>
+    <>
+      <View
+        className="h-full flex-1 px-4"
+        style={{
+          backgroundColor: colors.surface,
+          paddingTop: top,
+          paddingBottom: bottom,
+        }}
+      >
+        <View style={{ height: height / 1.25 }}>
+          <Surface elevation={4} className="rounded-lg overflow-hidden h-full">
+            <CameraView
+              ref={(ref) => setcamera(ref)}
+              videoQuality={videoQuality}
+              videoStabilizationMode="off"
+              facing={facing}
+              pictureSize={pictureSize}
+              mode={mode}
+              mute
+              className="w-full h-full"
+            />
+          </Surface>
+        </View>
+        <View className="flex-grow">
+          <View className="flex-row justify-center">
+            <Icon source="chevron-up" size={24} />
+            <IconButton icon="camera-reverse" onPress={showResDialog} />
+          </View>
+          <View className="flex-row flex-grow items-center justify-evenly">
+            <IconButton
+              icon={mode === "video" ? "camera" : "videocam"}
+              onPress={toggleCameraMode}
+            />
+            <TouchableOpacity
+              onPress={handleCapture}
+              className="w-20 h-20 rounded-full"
+              style={{
+                backgroundColor: colors.onSurfaceVariant,
+                borderWidth: 4,
+                borderColor: colors.outline,
+              }}
+            />
+            <IconButton icon="camera-reverse" onPress={toggleCameraFacing} />
+          </View>
+        </View>
       </View>
-      <View className="flex-row justify-center">
-        <IconButton icon="arrow-up" />
-      </View>
-      <View className="flex-row flex-grow items-center justify-evenly">
-        <IconButton
-          icon={mode === "video" ? "camera" : "videocam"}
-          onPress={toggleCameraMode}
-        />
-        <TouchableOpacity
-          onPress={handleCapture}
-          className="w-20 h-20 rounded-full"
-          style={{
-            backgroundColor: colors.onSurfaceVariant,
-            borderWidth: 4,
-            borderColor: colors.outline,
-          }}
-        />
-        <IconButton icon="camera-reverse" onPress={toggleCameraFacing} />
-      </View>
-    </View>
+      <Portal>
+        <Dialog
+          visible={isResDialogVisible}
+          onDismiss={hideResDialog}
+          style={{ maxHeight: "50%" }}
+        >
+          <Dialog.Title>Resolution</Dialog.Title>
+          <Dialog.ScrollArea>
+            <ScrollView>
+              <RadioButton.Group
+                value={pictureSize}
+                onValueChange={(value) => setpictureSize(value)}
+              >
+                {pictureSizes.map((item) => (
+                  <RadioButton.Item
+                    key={item.toString()}
+                    label={item}
+                    value={item}
+                  />
+                ))}
+              </RadioButton.Group>
+            </ScrollView>
+          </Dialog.ScrollArea>
+          <Dialog.Actions>
+            <Button onPress={hideResDialog}>Cancel</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </>
   );
 }
