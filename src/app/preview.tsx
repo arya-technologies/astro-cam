d38bf6e360a342af23acf2371b59edaca0016720
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-09 17:58:30 +0530

    optimized preview

--- src/app/preview.tsx
+++ src/app/preview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { router } from "expo-router";
 import { Appbar, IconButton } from "react-native-paper";
 import ImagePreview from "@/components/ImagePreview";
@@ -13,8 +13,10 @@ import PreviewMenu from "@/components/PreviewMenu";
 export default function preview() {
   const { colors } = useAppTheme();
   const { top, bottom } = useSafeAreaInsets();
+  const flatlist = useRef(null);
 
   const [assets, setassets] = useState<MediaLibrary.Asset[]>([]);
+  const [asset, setasset] = useState<MediaLibrary.Asset>();
 
   useEffect(() => {
     (async function () {
@@ -47,23 +49,26 @@ export default function preview() {
         <Appbar.Action icon="ellipsis-vertical" />
       </Appbar.Header>
       <FlatList
+        ref={flatlist}
         horizontal
         snapToAlignment="center"
         pagingEnabled
         viewabilityConfig={{ itemVisiblePercentThreshold: 90 }}
         data={assets}
-        renderItem={({ item }) => (
-          <>
-            {item.mediaType === "video" ? (
-              <VideoPreview key={item.id} videoUri={item.uri} />
-            ) : (
-              <ImagePreview key={item.id} imageUri={item.uri} />
-            )}
-            <PreviewMenu asset={item} />
-          </>
-        )}
+        initialNumToRender={1}
+        renderItem={({ item }) => renderItem(item)}
         className="absolute h-full -z-10"
+        onViewableItemsChanged={({ changed }) => setasset(changed[0].item)}
       />
+      {asset && <PreviewMenu asset={asset} />}
     </View>
   );
 }
+
+const renderItem = (item: MediaLibrary.Asset) => {
+  return item.mediaType === "video" ? (
+    <VideoPreview key={item.id} videoUri={item.uri} />
+  ) : (
+    <ImagePreview key={item.id} imageUri={item.uri} />
+  );
+};
