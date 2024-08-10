753132429e21896b72dff3ab2f64aed07c7d6064
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-10 22:04:07 +0530

    up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,83 +1,55 @@
-import { useAppTheme } from "@/components/providers/Material3ThemeProvider";
-import Slider from "@/components/Slider";
 import {
-  PictureSizeProps,
-  RatioProps,
-  setcontrols,
-} from "@/features/slices/settingsSlice";
-import { RootState } from "@/features/store";
-import {
-  CameraMode,
-  CameraView,
-  ImageType,
-  VideoQuality,
-  VideoStabilization,
-  useCameraPermissions,
-} from "expo-camera";
-import * as MediaLibrary from "expo-media-library";
+  Camera,
+  CameraDevice,
+  useCameraDevice,
+  useCameraDevices,
+  useCameraFormat,
+  useCameraPermission,
+} from "react-native-vision-camera";
 import { router } from "expo-router";
-import { useEffect, useState } from "react";
-import {
-  Image,
-  Linking,
-  Pressable,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import {
-  Button,
-  Dialog,
-  IconButton,
-  List,
-  Portal,
-  RadioButton,
-  Text,
-} from "react-native-paper";
+import { useEffect, useRef, useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import { Linking, View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { useAppTheme } from "@/components/providers/Material3ThemeProvider";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/features/store";
 
 export default function index() {
   const { colors } = useAppTheme();
   const { top, bottom } = useSafeAreaInsets();
   const dispatch = useDispatch();
   const { controls } = useSelector((state: RootState) => state.settings);
-  const [permission, requestPermission] = useCameraPermissions();
+  const { hasPermission, requestPermission } = useCameraPermission();
   const [mediaPermission, requestMediaPermission] =
     MediaLibrary.usePermissions();
 
-  const [isResDialogVisible, setisResDialogVisible] = useState<boolean>(false);
-  const showResDialog = () => setisResDialogVisible(true);
-  const hideResDialog = () => setisResDialogVisible(false);
-  const [isPictureTypesDialogVisible, setisPictureTypesDialogVisible] =
-    useState<boolean>(false);
-  const showPictureTypesDialog = () => setisPictureTypesDialogVisible(true);
-  const hidePictureTypesDialog = () => setisPictureTypesDialogVisible(false);
+  const camera = useRef<Camera>(null);
+  const devices = useCameraDevices();
+  const device: CameraDevice = useCameraDevice("back", {
+    physicalDevices: [
+      "ultra-wide-angle-camera",
+      "wide-angle-camera",
+      "telephoto-camera",
+    ],
+  });
+  const usbCamera = useCameraDevice("external");
+  const format = useCameraFormat(device, []);
+  const [fps, setfps] = useState(format?.maxFps);
 
-  const [mode, setmode] = useState<CameraMode>(controls?.mode);
-  const [camera, setcamera] = useState<CameraView | null>();
-  const [pictureSize, setpictureSize] = useState<PictureSizeProps>(
-    controls?.pictureSize,
-  );
-  const [pictureSizes, setpictureSizes] = useState<string[]>([]);
-  const [imageType, setimageType] = useState<ImageType>(controls?.imageType);
-  const [imageTypes, setimageTypes] = useState<ImageType[]>(["png", "jpg"]);
-  const [iso, setiso] = useState<number>(0);
-  const [exposure, setexposure] = useState<number>(0);
-  const [zoom, setzoom] = useState<number>(0);
-  const [ratio, setratio] = useState<RatioProps>(controls?.ratio);
-  const [videoQuality, setvideoQuality] = useState<VideoQuality>(
-    controls?.videoQuality,
-  );
-  const videoQualities: VideoQuality[] = ["480p", "720p", "1080p", "2160p"];
-  const [videoStabilization, setvideoStabilization] =
-    useState<VideoStabilization>(controls?.videoStabilization);
+  // const isFocused = useIsFocused()
+  //   const appState = useAppState()
+  //   const isActive = isFocused && appState === "active"
+
+  const [mode, setmode] = useState<"picture" | "video">();
 
   const [lastCapturedUri, setlastCapturedUri] = useState<string>();
   const [isrecording, setisrecording] = useState<boolean>(false);
 
   useEffect(() => {
     (async function () {
-      if (permission?.granted && mediaPermission?.granted) {
+      if (hasPermission && mediaPermission?.granted) {
         const album = await MediaLibrary.getAlbumAsync("AstroCam");
         if (album) {
           const albumAssets = await MediaLibrary.getAssetsAsync({
@@ -89,30 +61,26 @@ export default function index() {
             setlastCapturedUri(albumAssets.assets[0]?.uri);
           }
         }
-
-        const pictureSizesRes = await camera?.getAvailablePictureSizesAsync();
-        if (pictureSizesRes) {
-          setpictureSizes(pictureSizesRes);
-        }
       }
     })();
   }, [camera]);
 
-  useEffect(() => {
-    dispatch(
-      setcontrols({
-        mode,
-        imageType,
-        pictureSize,
-        ratio,
-        videoQuality,
-        videoStabilization,
-      }),
-    );
-  }, [mode, imageType, pictureSize, ratio, videoQuality, videoStabilization]);
+  // useEffect(() => {
+  //   dispatch(
+  //     setcontrols({
+  //       mode,
+  //       imageType,
+  //       pictureSize,
+  //       ratio,
+  //       videoQuality,
+  //       videoStabilization,
+  //     }),
+  //   );
+  // }, [mode, imageType, pictureSize, ratio, videoQuality, videoStabilization]);
+  //
 
   const requestPermissions = () => {
-    if (!permission?.granted) {
+    if (!hasPermission) {
       requestPermission();
     }
     if (!mediaPermission?.granted) {
@@ -120,37 +88,62 @@ export default function index() {
     }
   };
 
-  function toggleCameraMode() {
-    setmode((current) => (current === "picture" ? "video" : "picture"));
-    setzoom(0);
+  if (!hasPermission) {
+    const handleRequestPermissions = () => {
+      if (!hasPermission && mediaPermission?.canAskAgain) {
+        requestPermissions();
+      } else {
+        Linking.openSettings();
+      }
+    };
+    return (
+      <View
+        className="h-full flex-1 items-center justify-center space-y-4"
+        style={{ backgroundColor: colors.surface }}
+      >
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={handleRequestPermissions} mode="elevated">
+          Grant Permission
+        </Button>
+      </View>
+    );
   }
 
-  async function handleCapture() {
+  const ensureDirExist = async () => {
+    const album = await MediaLibrary.getAlbumAsync("AstroCam");
+    if (!album) {
+      const res = await MediaLibrary.createAlbumAsync("AstroCam");
+      if (res) {
+        return true;
+      }
+    }
+    return false;
+  };
+  const handleCapture = async () => {
+    const album = await MediaLibrary.getAlbumAsync("AstroCam");
     if (mode === "picture") {
-      const data = await camera?.takePictureAsync({
-        imageType,
-        quality: 1,
-        skipProcessing: true,
-      });
-      setlastCapturedUri(data?.uri);
+      const data = await camera.current?.takePhoto({ path: "AstroCam" });
       if (data) {
-        addImage(data.uri);
+        setlastCapturedUri(data?.path);
       }
     } else if (mode === "video") {
       if (!isrecording) {
         setisrecording(true);
-        const data = await camera?.recordAsync({});
-        setlastCapturedUri(data?.uri);
+        const data = await camera.current?.startRecording({
+          path: "AstroCam",
+          onRecordingFinished(video) {
+            setlastCapturedUri(video.path);
+          },
+        });
+        // setlastCapturedUri(data);
         if (data) {
-          addVideo(data.uri);
         }
       } else {
-        camera?.stopRecording();
+        camera.current?.stopRecording();
         setisrecording(false);
       }
     }
-  }
-
+  };
   async function addImage(imageUri: string) {
     const imagesDir = "AstroCam";
     const asset = await MediaLibrary.createAssetAsync(imageUri);
@@ -172,27 +165,6 @@ export default function index() {
     }
   }
 
-  if (!permission?.granted || !mediaPermission?.granted) {
-    const handleRequestPermissions = () => {
-      if (permission?.canAskAgain && mediaPermission?.canAskAgain) {
-        requestPermissions();
-      } else {
-        Linking.openSettings();
-      }
-    };
-    return (
-      <View
-        className="h-full flex-1 items-center justify-center space-y-4"
-        style={{ backgroundColor: colors.surface }}
-      >
-        <Text>We need your permission to show the camera</Text>
-        <Button onPress={handleRequestPermissions} mode="elevated">
-          Grant Permission
-        </Button>
-      </View>
-    );
-  }
-
   return (
     <>
       <View
@@ -204,140 +176,23 @@ export default function index() {
         }}
       >
         <View style={{}} className="items-center">
-          <CameraView
-            mute
-            mode={mode}
-            zoom={zoom}
-            facing="back"
-            autofocus="on"
-            pictureSize={pictureSize}
-            videoQuality={videoQuality}
-            videoStabilizationMode={videoStabilization}
-            ref={(ref) => setcamera(ref)}
+          <Camera
+            isActive
+            ref={camera}
+            device={device}
+            format={format}
+            fps={fps}
+            photo={true}
+            video={true}
+            photoHdr={false}
+            videoHdr={false}
+            videoStabilizationMode="off"
+            resizeMode="contain"
+            androidPreviewViewType="surface-view"
             className="w-[95vw] h-[95vw] my-[5vw]"
           />
         </View>
-        <View className="flex-grow">
-          <View className="flex-row flex-grow">
-            <View className="items-center justify-end flex-grow p-2 space-y-2">
-              {mode === "video" ? (
-                <>
-                  <IconButton
-                    icon="image"
-                    mode="contained"
-                    onPress={showResDialog}
-                  />
-                </>
-              ) : (
-                <>
-                  <IconButton
-                    icon="image"
-                    mode="contained"
-                    onPress={showPictureTypesDialog}
-                  />
-                </>
-              )}
-              <IconButton
-                icon="settings"
-                mode="contained"
-                onPress={() => router.navigate("settings")}
-              />
-            </View>
-            <View className="flex-grow justify-end">
-              <List.Section>
-                <List.Item
-                  title="Zoom"
-                  right={() => <Text>{(zoom * 10).toPrecision(2)}x</Text>}
-                />
-                <Slider
-                  minValue={0}
-                  maxValue={1}
-                  step={0.1}
-                  value={zoom}
-                  onValueChange={setzoom}
-                />
-              </List.Section>
-            </View>
-          </View>
-          <View className="flex-row items-center justify-evenly py-4">
-            <Pressable onPress={() => router.navigate("preview")}>
-              <Image
-                source={
-                  lastCapturedUri
-                    ? {
-                        uri: lastCapturedUri,
-                      }
-                    : require("../../assets/icon.png")
-                }
-                className="w-16 h-16 rounded-full"
-              />
-            </Pressable>
-            <TouchableOpacity
-              onPress={handleCapture}
-              className="w-20 h-20 rounded-full"
-              style={{
-                backgroundColor: isrecording
-                  ? colors.scrim
-                  : colors.onSurfaceVariant,
-                borderWidth: 4,
-                borderColor: colors.outline,
-              }}
-            />
-            <IconButton
-              size={40}
-              icon={mode === "video" ? "camera" : "videocam"}
-              onPress={toggleCameraMode}
-            />
-          </View>
-        </View>
       </View>
-      <Portal>
-        <Dialog visible={isResDialogVisible} onDismiss={hideResDialog}>
-          <Dialog.Title>Resolution</Dialog.Title>
-          <Dialog.Content>
-            <RadioButton.Group
-              value={videoQuality}
-              onValueChange={(value: VideoQuality) => setvideoQuality(value)}
-            >
-              {videoQualities.map((item) => (
-                <RadioButton.Item
-                  key={item.toString()}
-                  label={item}
-                  value={item}
-                />
-              ))}
-            </RadioButton.Group>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hideResDialog}>Cancel</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
-      <Portal>
-        <Dialog
-          visible={isPictureTypesDialogVisible}
-          onDismiss={hidePictureTypesDialog}
-        >
-          <Dialog.Title>Image Types</Dialog.Title>
-          <Dialog.Content>
-            <RadioButton.Group
-              value={imageType}
-              onValueChange={(value: ImageType) => setimageType(value)}
-            >
-              {imageTypes.map((item) => (
-                <RadioButton.Item
-                  key={item.toString()}
-                  label={item}
-                  value={item}
-                />
-              ))}
-            </RadioButton.Group>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hidePictureTypesDialog}>Cancel</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
     </>
   );
 }
