097d1ace380711cb3dae2d141aa12afea3717284
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-17 20:32:27 +0530

    qdded gesture handler

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,8 @@ import { persistor, store } from "@/features/store";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router/stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ActivityIndicator } from "react-native-paper";
-import { ReduceMotion } from "react-native-reanimated";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
@@ -20,12 +20,14 @@ export default function Layout() {
             icon: (props: any) => <Ionicons {...props} />,
           }}
         >
-          <Stack
-            screenOptions={{
-              animation: "default",
-              headerShown: false,
-            }}
-          />
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack
+              screenOptions={{
+                animation: "default",
+                headerShown: false,
+              }}
+            />
+          </GestureHandlerRootView>
         </Material3ThemeProvider>
       </PersistGate>
     </Provider>
